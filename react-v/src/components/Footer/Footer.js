c791e16bae456f7fc38f4b76a14549f96f0a73ee
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2022-09-09 16:15:32 -0400

    Transform to react app

---
+++ react-v/src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <p>
+          Gladston Aristoverne | 2020 - Formation{" "}
+          <a href="https://openclassrooms.com">Openclassrooms</a>
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ react-v/src/components/Realisations/Realisations.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Relisations = () => {
+  return (
+    <div>
+      <section className="achievements">
+        <h2>
+          <i className="fas fa-pencil-alt"></i> RÉALISATIONS
+        </h2>
+        <div className="divider"></div>
+        <ul>
+          <li>
+            AJ Loc & Services - Intégration sous WordPress -
+            <a href="https://www.ajlocationetservices.fr/">Voir le projet</a>.
+          </li>
+          <li>
+            Kbatis - Intégration sous WordPress -
+            <a href="https://www.kbatis.com/">Voir le projet</a>.
+          </li>
+          <li>
+            Ohmyfood - Intégration de maquette -
+            <a href="https://ag-ohmyfood.netlify.app/">Voir le projet</a>.
+          </li>
+          <li>
+            QuentinChantrel - Intégration sous WordPress -
+            <a href="https://quentinchantrel.com/">Voir le projet</a>.
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export default Relisations;
